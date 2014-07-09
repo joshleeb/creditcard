3bc8356571fb716bfef3c715e60d7432bd3c92f0
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2014-07-09 16:56:58 +1000

    luhn.py : Refactoring

--- cc_val/luhn.py
+++ cc_val/luhn.py
@@ -2,25 +2,29 @@ class Luhn:
   def __init__(self):
     pass
 
-  def digits_of(self, n):
-    return [int(d) for d in str(n)]
-
   def calculate_check_digit(self, seq):
     digits, checksum = self.digits_of(seq), 0
-    odd_indexes, even_indexes = digits[-1::-2], digits[-2::-2]
+    even_digits, odd_digits = self.even_loc(digits), self.odd_loc(digits)
 
-    checksum += sum(even_indexes)
-    checksum += sum([sum(self.digits_of(2*d)) for d in odd_indexes])
+    checksum += sum(even_digits)
+    checksum += sum([sum(self.digits_of(2*d)) for d in odd_digits])
     check_digit = 9 * checksum % 10
 
     return check_digit
 
   def is_valid(self, seq):
-    if isinstance(seq, str) and seq.isdigit():
-      pass
-    elif isinstance(seq, int) and seq > 0:
-      seq = str(seq)
-    else:
+    seq = str(seq)
+    if not seq.isdigit():
       raise TypeError("Sequence must be of type: str or int")
-
     return int(seq[-1]) == self.calculate_check_digit(seq[:-1])
+
+  def digits_of(self, n):
+    return [int(d) for d in str(n)]
+
+  def even_loc(self, seq):
+    """ Returns a list of the element of even indexes within seq """
+    return seq[-2::-2]
+
+  def odd_loc(self, seq):
+    """ Returns a list of the element of odd indexes within seq """
+    return seq[-1::-2]
