68f3440a19ccdf600082703350c0a5f92b0b25f0
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2014-07-11 11:34:03 +1000

    luhn.py : If no length is passed for generating numbers, a random length is selected

--- creditcard/luhn.py
+++ creditcard/luhn.py
@@ -37,15 +37,16 @@ class LuhnGenerator:
   def __init__(self):
     self.luhn_validator = LuhnValidator()
 
-  def generate(self, length):
+  def generate(self, length=None):
     number_list = []
 
+    if length is None:
+      length = random.randint(2, 30)
     if not isinstance(length, int) or length < 2:
       raise TypeError("Length must be a positive integer > 1.")
 
     for i in range(length-1):
       number_list.append(str(random.randint(0, 9)))
-
     number = ''.join(number_list)
     number_list.append(self.luhn_validator.calculate_check_digit(number))
 
--- tests/test_luhn_generator.py
+++ tests/test_luhn_generator.py
@@ -5,6 +5,9 @@ import random
 
 
 class TestLuhnGenerator:
+
+  MULTIPLIER = 3
+
   def __init__(self):
     self.luhn_val = LuhnValidator()
     self.luhn_gen = LuhnGenerator()
@@ -12,9 +15,13 @@ class TestLuhnGenerator:
     self.invalid_length_types = [0, 1, -1, "123", 2.1]
 
   def test_generates_valid_number_of_given_length(self):
-    for i in range(3):
-      length = random.randint(2, 20)
+    for i in range(TestLuhnGenerator.MULTIPLIER):
+      length = random.randint(2, 30)
       yield self.check_generates_valid_number_of_given_length, length
+
+  def test_no_length_generates_number_of_random_length(self):
+    for i in range(TestLuhnGenerator.MULTIPLIER):
+      yield self.check_generates_valid_number_of_random_length
 
   def test_invalid_length_type_raises_typeerror(self):
     for t in self.invalid_length_types:
@@ -23,8 +30,12 @@ class TestLuhnGenerator:
   def check_generates_valid_number_of_given_length(self, length):
     generated_number = self.luhn_gen.generate(length)
     assert self.luhn_val.is_valid(generated_number) is True
+    assert len(str(generated_number)) == length
+
+  def check_generates_valid_number_of_random_length(self):
+    generated_number = self.luhn_gen.generate()
+    assert self.luhn_val.is_valid(generated_number) is True
 
   @raises(TypeError)
   def check_invalid_length_type_raises_typeerror(self, length):
     self.luhn_gen.generate(length)
-
