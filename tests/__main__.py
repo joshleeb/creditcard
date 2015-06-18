b76c4823fc0c1e7ac7636725f74ed67054677970
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2015-06-18 16:17:05 +1000

    Change specs from PyTest to Unittest
    Added `__main__.py` to tests directory for easier running of test with
    `python tests`. Moves formatter specs from using PyTest to Python's
    Unittest.

---
+++ tests/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+#from luhn_spec import TestFormatter, TestGenerator
+from formatter_spec import TestFormatter
+
+
+if __name__ == '__main__':
+    unittest.main()
