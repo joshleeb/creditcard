b646aea790a93af1ae77b7c35c58d03c7a55d4a6
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2015-06-18 17:31:34 +1000

    Refactor of formatter and specs removing unnecesary class

--- creditcard/formatter.py
+++ creditcard/formatter.py
@@ -1,95 +1,92 @@
-class Formatter:
-    """
-    Determines the format, or possible formats, of a given credit card number.
-    """
+def is_visa(n):
+    """Checks if credit card number fits the visa format."""
+    n, length = str(n), len(str(n))
 
-    def __init__(self):
-        pass
+    if length >= 13 and length <= 16:
+        if n[0] == '4':
+            return True
+    return False
 
-    def is_visa(self, n):
-        """Checks if credit card number fits the visa format."""
-        n, length = str(n), len(str(n))
 
-        if length >= 13 and length <= 16:
-            if n[0] == "4":
+def is_visa_electron(n):
+    """Checks if credit card number fits the visa electron format."""
+    n, length = str(n), len(str(n))
+    form = ['026', '508', '844', '913', '917']
+
+    if length == 16:
+        if n[0] == '4':
+            if ''.join(n[1:4]) in form or ''.join(n[1:6]) == '17500':
                 return True
-        return False
+    return False
 
-    def is_visa_electron(self, n):
-        """Checks if credit card number fits the visa electron format."""
-        n, length = str(n), len(str(n))
-        form = ["026", "508", "844", "913", "917"]
 
-        if length == 16:
-            if n[0] == "4":
-                if ''.join(n[1:4]) in form or ''.join(n[1:6]) == "17500":
-                    return True
-        return False
+def is_mastercard(n):
+    """Checks if credit card number fits the mastercard format."""
+    n, length = str(n), len(str(n))
 
-    def is_mastercard(self, n):
-        """Checks if credit card number fits the mastercard format."""
-        n, length = str(n), len(str(n))
+    if length >= 16 and length <= 19:
+        if ''.join(n[:2]) in strings_between(51, 56):
+            return True
+    return False
 
-        if length >= 16 and length <= 19:
-            if ''.join(n[:2]) in self.strings_between(51, 56):
+
+def is_amex(n):
+    """Checks if credit card number fits the american express format."""
+    n, length = str(n), len(str(n))
+
+    if length == 15:
+        if n[0] == '3' and (n[1] == '4' or n[1] == '7'):
+            return True
+    return False
+
+
+def is_maestro(n):
+    """Checks if credit card number fits the maestro format."""
+    n, length = str(n), len(str(n))
+    form = ['5018', '5020', '5038', '5893', '6304',
+                    '6759', '6761', '6762', '6763']
+
+    if length >= 16 and length <= 19:
+        if ''.join(n[:4]) in form:
+            return True
+    return False
+
+
+def is_discover(n):
+    """Checks if credit card number fits the discover card format."""
+    n, length = str(n), len(str(n))
+
+    if length == 16:
+        if n[0] == '6':
+            if ''.join(n[1:4]) == '011' or n[1] == '5':
                 return True
-        return False
-
-    def is_amex(self, n):
-        """Checks if credit card number fits the american express format."""
-        n, length = str(n), len(str(n))
-
-        if length == 15:
-            if n[0] == "3" and (n[1] == "4" or n[1] == "7"):
+            elif n[1] == '4' and n[2] in strings_between(4, 10):
                 return True
-        return False
-
-    def is_maestro(self, n):
-        """Checks if credit card number fits the maestro format."""
-        n, length = str(n), len(str(n))
-        form = ["5018", "5020", "5038", "5893", "6304",
-                        "6759", "6761", "6762", "6763"]
-
-        if length >= 16 and length <= 19:
-            if ''.join(n[:4]) in form:
+            elif ''.join(n[1:6]) in strings_between(22126, 22926):
                 return True
-        return False
+    return False
 
-    def is_discover(self, n):
-        """Checks if credit card number fits the discover card format."""
-        n, length = str(n), len(str(n))
 
-        if length == 16:
-            if n[0] == "6":
-                if ''.join(n[1:4]) == "011" or n[1] == "5":
-                    return True
-                elif n[1] == "4" and n[2] in self.strings_between(4, 10):
-                    return True
-                elif ''.join(n[1:6]) in self.strings_between(22126, 22926):
-                    return True
-        return False
+def get_format(n):
+    """Gets a list of the formats a credit card number fits."""
+    formats = []
 
-    def get_format(self, n):
-        """Gets a list of the formats a credit card number fits."""
-        formats = []
+    if is_visa(n):
+        formats.append('visa')
+    if is_visa_electron(n):
+        formats.append('visa electron')
+    if is_mastercard(n):
+        formats.append('mastercard')
+    if is_amex(n):
+        formats.append('amex')
+    if is_maestro(n):
+        formats.append('maestro')
+    if is_discover(n):
+        formats.append('discover')
 
-        if self.is_visa(n):
-            formats.append("VISA")
-        if self.is_visa_electron(n):
-            formats.append("VISA ELECTRON")
-        if self.is_mastercard(n):
-            formats.append("MASTERCARD")
-        if self.is_amex(n):
-            formats.append("AMEX")
-        if self.is_maestro(n):
-            formats.append("MAESTRO")
-        if self.is_discover(n):
-            formats.append("DISCOVER")
+    return formats
 
-        if len(formats) == 0:
-            return ["Unknown"]
-        return formats
 
-    def strings_between(self, a, b):
-        """Generates a list of strings between a and b."""
-        return list(map(str, range(a, b)))
+def strings_between(a, b):
+    """Generates a list of strings between a and b."""
+    return list(map(str, range(a, b)))
--- tests/formatter_spec.py
+++ tests/formatter_spec.py
@@ -1,100 +1,68 @@
-# TODO: add tests for error handling
-
 import os
 import sys
 import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from creditcard.formatter import Formatter
+from creditcard import formatter
 
 
 class TestFormatter(unittest.TestCase):
     def test_should_identify_visa_number(self):
         visa_number = 4024007183310266
-        formatter = Formatter()
-
         self.assertTrue(formatter.is_visa(visa_number))
 
     def test_should_identify_visa_number_string(self):
-        visa_string = "4024007183310266"
-        formatter = Formatter()
-
+        visa_string = '4024007183310266'
         self.assertTrue(formatter.is_visa(visa_string))
 
     def test_should_identify_visa_electron_number(self):
         visa_electron_number = 4175004688713760
-        formatter = Formatter()
-
         self.assertTrue(formatter.is_visa_electron(visa_electron_number))
 
     def test_should_identify_visa_electron_number_string(self):
-        visa_electron_string = "4175004688713760"
-        formatter = Formatter()
-
+        visa_electron_string = '4175004688713760'
         self.assertTrue(formatter.is_visa_electron(visa_electron_string))
 
     def test_should_identify_mastercard_number(self):
         mastercard_number = 5409219472999830
-        formatter = Formatter()
-
         self.assertTrue(formatter.is_mastercard(mastercard_number))
 
     def test_should_identify_mastercard_number_string(self):
-        mastercard_string = "5409219472999830"
-        formatter = Formatter()
-
+        mastercard_string = '5409219472999830'
         self.assertTrue(formatter.is_mastercard(mastercard_string))
 
     def test_should_identify_amex_number(self):
         amex_number = 374619657687666
-        formatter = Formatter()
-
         self.assertTrue(formatter.is_amex(amex_number))
 
     def test_should_identify_amex_number_string(self):
-        amex_string = "374619657687666"
-        formatter = Formatter()
-
+        amex_string = '374619657687666'
         self.assertTrue(formatter.is_amex(amex_string))
 
     def test_should_identify_maestro_number(self):
         maestro_number = 6304236404755563
-        formatter = Formatter()
-
         self.assertTrue(formatter.is_maestro(maestro_number))
 
     def test_should_identify_maestro_number_string(self):
-        maestro_string = "6304236404755563"
-        formatter = Formatter()
-
+        maestro_string = '6304236404755563'
         self.assertTrue(formatter.is_maestro(maestro_string))
 
     def test_should_identify_discover_number(self):
         discover_number = 6011359876556543
-        formatter = Formatter()
-
         self.assertTrue(formatter.is_discover(discover_number))
 
     def test_should_identify_discover_number_string(self):
-        discover_string = "6011359876556543"
-        formatter = Formatter()
-
+        discover_string = '6011359876556543'
         self.assertTrue(formatter.is_discover(discover_string))
 
     def test_should_return_none_for_unknown_formats(self):
         unknown = 1234567890
-        formatter = Formatter()
-
-        self.assertEqual(formatter.get_format(unknown), ['Unknown'])
+        self.assertEqual(formatter.get_format(unknown), [])
 
     def test_should_get_single_format_of_number(self):
         number = 5409219472999830
-        formatter = Formatter()
-
-        self.assertEqual(formatter.get_format(number), ['MASTERCARD'])
+        self.assertEqual(formatter.get_format(number), ['mastercard'])
 
     def test_should_get_both_formats_of_number(self):
         number = 4508077077058854
-        formatter = Formatter()
-
-        self.assertEqual(formatter.get_format(number), ['VISA', 'VISA ELECTRON'])
+        self.assertEqual(formatter.get_format(number), ['visa', 'visa electron'])
